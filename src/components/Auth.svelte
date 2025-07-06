e3e2b75af7d16a33c988d0a8e9a38977ef117601
Scythe101 <julianchenard@icloud.com>
2025-07-05 22:15:35 -0700

    auth

---
+++ src/components/Auth.svelte
@@ -0,0 +1,23 @@
+<script>
+    let register = false;
+    let email = '';
+    let password = '';
+    let confirmPassword = '';
+</script>
+<div>
+    <h1>Sign Up</h1>
+    <form>
+        <label>
+            <input type="text" placeholder="Email"/>
+        </label>
+        <label>
+            <input type="password" placeholder="Password"/>
+        </label>
+        {#if register}
+            <label>
+                <input type="password" placeholder="Confirm Password"/>
+            </label>
+        {/if}
+        <button type="submit">Sign Up</button>
+    </form>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '../app.css';
+	
+
+	let { children } = $props();
+</script>
+
+<main class="bg-amber-50 min-h-dvh">
+	{@render children()}
+</main>
