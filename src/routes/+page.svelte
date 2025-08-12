76073137f758b98a1c3a945d31dd87ab442676f3
Julian Chenard <julianchenard@icloud.com>
2025-08-11 21:38:16 -0700

    started finishing touches for release

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,15 @@
 <h1 class="mt-24 text-center text-6xl">Class planning made easy.</h1>
-<p class="mt-6 text-center text-4xl">One place for all your academic planning needs.</p>
+<h2 class="font-noto-serif mt-6 text-center text-4xl not-italic">
+	One place for all your academic planning needs.
+</h2>
+
 <div class="font-noto-serif mt-12 flex justify-center">
 	<a
-		class="font-noto-serif shadow-sharp hover:shadow-sharp-hover mt-8 flex h-12 w-48 cursor-pointer flex-row items-center justify-center gap-3 rounded-full border-2 border-slate-900 bg-white px-4 py-2 text-lg font-semibold shadow-slate-900 transition-all duration-200 hover:-translate-0.5"
+		class="font-noto-serif shadow-sharp-chonk hover:shadow-sharp-chonk-hover mt-8 flex h-50 w-300 cursor-pointer flex-row items-center justify-center gap-3 rounded-full border-2 border-slate-900 bg-white px-4 py-2 text-lg font-semibold shadow-slate-900 transition-all duration-200 hover:-translate-2.5"
 		href="/auth">Start now</a
 	>
 </div>
+<p class="mt-30 text-center text-xl">
+	Instead of hopping between a Google Doc, Aeries, and the counselors' website to plan out your
+	schedule, just use Raven Planner!
+</p>
--- src/routes/app/info/+page.svelte
+++ src/routes/app/info/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { auth } from '$lib/firebase/firebase.client';
+	import { auth } from '$lib/firebase/firebase.client';
 	import { authHandlers } from '$stores/AuthStore';
 	import { authStore } from '$stores/AuthStore';
 	import { goto } from '$app/navigation';
@@ -38,33 +38,87 @@
 			hasInitializedSelection = true;
 		}
 	});
-
-	
 </script>
 
 <h1>Here's some information you should know about this website!</h1>
 <h2 class="mt-4">Tips</h2>
-<p class="text-xl">I made this on a big monitor, so the text scaling is really big on chromebooks (and smaller screens in general). I recommend pressing ctrl + plus or minus until the website is at a good size for you, which should help resolve the problem until I actually get around to fixing it.</p>
-<p class="text-xl mt-2">If you are in Conservatory or have <a target="_blank" class="text-sky-700 underline" href="http://www.ccaravensathletics.com/pe-credit.html">athletics PE credit</a>, press the "+" button in the top right corner of the course selection tiles to be able to add it to your schedule planner!</p>
-<img class="h-60" src="/add_course_example.png" alt="Example of + button">
+<p class="text-xl">
+	I made this on a big monitor, so the text scaling is really big on chromebooks (and smaller
+	screens in general). I recommend pressing ctrl + plus or minus until the website is at a good size
+	for you, which should help resolve the problem until I actually get around to fixing it.
+</p>
+<p class="mt-2 text-xl">
+	If you are in Conservatory or have <a
+		target="_blank"
+		class="text-sky-700 underline"
+		href="http://www.ccaravensathletics.com/pe-credit.html">athletics PE credit</a
+	>, press the "+" button in the top right corner of the course selection tiles to be able to add it
+	to your schedule planner!
+</p>
+<img class="h-60" src="/add_course_example.png" alt="Example of + button" />
 
 <h2 class="mt-4">Warnings</h2>
-<p class="text-xl">This should not be taken as academic advice, it's just a tool to help you plan. If you are in need of academic help, <a target="_blank" class="underline text-sky-700" href="https://sites.google.com/sduhsd.net/cca-counseling/home">please reach out to a counselor</a>.</p>
-<p class="text-xl mt-2">I cannot gurantee that my code will work 100% of the time, especially since this is my first time tackling a big project.</p>
-<p class="text-xl mt-2">All the course profile information was copied directly from the <a target="_blank" class="underline text-sky-700" href="https://sites.google.com/sduhsd.net/cca-counseling/home">counselors' website</a>, and some information may be dependent on the teacher.</p>
-<p class="text-xl mt-2">Although the majority of the code was written by me, some of it was written with the help of (not directly by) AI. The exception to this is the course profiles, as I am not spending 16 hours painstakingly copying over 150+ course profiles.</p>
-<p class="text-xl mt-2">If you run into any issues, let me know or open an issue on GitHub.</p>
+<p class="text-xl">
+	This should not be taken as academic advice, it's just a tool to help you plan. If you are in need
+	of academic help, <a
+		target="_blank"
+		class="text-sky-700 underline"
+		href="https://sites.google.com/sduhsd.net/cca-counseling/home"
+		>please reach out to a counselor</a
+	>.
+</p>
+<p class="mt-2 text-xl">
+	I cannot gurantee that my code will work 100% of the time, especially since this is my first time
+	tackling a big project.
+</p>
+<p class="mt-2 text-xl">
+	All the course profile information was copied directly from the <a
+		target="_blank"
+		class="text-sky-700 underline"
+		href="https://sites.google.com/sduhsd.net/cca-counseling/home">counselors' website</a
+	>, and some information may be dependent on the teacher.
+</p>
+<p class="mt-2 text-xl">
+	Although the majority of the code was written by me, some of it was written with the help of (not
+	directly by) AI. The exception to this is the course profiles, as I am not spending 16 hours
+	painstakingly copying over 150+ course profiles.
+</p>
+<p class="mt-2 text-xl">
+	This doesn't stop you from putting courses in the wrong slot (ie English 9 in sophomore year) so
+	look at the prerequisites in the course profiles.
+</p>
 
 <h2 class="mt-4">Technical Info (feel free to skip if you're not interested)</h2>
-<p class="text-xl">The source code for this whole project is on <a target="_blank" href="https://github.com/Scythe101/raven-planner" class="underline text-sky-700">GitHub</a>!</p>
-<p class="text-xl mt-2">This took me around a month to build.</p>
-<p class="text-xl mt-2 mb-4"><a target="_blank" href="https://svelte.dev/" class="underline text-sky-700">Svelte</a> is my framework of choice, <a target="_blank" href="https://firebase.google.com" class="underline text-sky-700">Firebase</a> is the database and authentication, <a target="_blank" href="https://figma.com/" class="underline text-sky-700">Figma</a> was used for designing, and <a target="_blank" href="https://code.visualstudio.com/" class="underline text-sky-700">VS Code</a> and <a target="_blank" href="https://zed.dev/" class="underline text-sky-700">Zed</a> are where this website was written.</p>
+<p class="text-xl">
+	The source code for this whole project is on <a
+		target="_blank"
+		href="https://github.com/Scythe101/raven-planner"
+		class="text-sky-700 underline">GitHub</a
+	>!
+</p>
+<p class="mt-2 text-xl">This took me around a month to build.</p>
+<p class="mt-2 mb-4 text-xl">
+	<a target="_blank" href="https://svelte.dev/" class="text-sky-700 underline">Svelte</a> is my
+	framework of choice,
+	<a target="_blank" href="https://firebase.google.com" class="text-sky-700 underline">Firebase</a>
+	is the database and authentication,
+	<a target="_blank" href="https://figma.com/" class="text-sky-700 underline">Figma</a>
+	was used for designing, and
+	<a target="_blank" href="https://code.visualstudio.com/" class="text-sky-700 underline">VS Code</a
+	>
+	and <a target="_blank" href="https://zed.dev/" class="text-sky-700 underline">Zed</a> are where this
+	website was written.
+</p>
 
 {#if !info || info?.settings?.newUser}
-	<button onclick={() => {
-		info.settings.newUser = false;
-		userData.set(info);
-		saveUserData();
-		goto('/app');
-	}} class="ml-0.5 font-noto-serif shadow-sharp hover:shadow-sharp-hover mt-8 flex h-12 w-fit cursor-pointer flex-row items-center justify-center gap-3 rounded-full border-2 border-slate-900 bg-white px-4 py-2 text-lg font-semibold shadow-slate-900 transition-all duration-200 hover:-translate-0.5 mb-4">I understand and read everything, let me access the website.</button>
+	<button
+		onclick={() => {
+			info.settings.newUser = false;
+			userData.set(info);
+			saveUserData();
+			goto('/app');
+		}}
+		class="font-noto-serif shadow-sharp hover:shadow-sharp-hover mt-8 mb-4 ml-0.5 flex h-12 w-fit cursor-pointer flex-row items-center justify-center gap-3 rounded-full border-2 border-slate-900 bg-white px-4 py-2 text-lg font-semibold shadow-slate-900 transition-all duration-200 hover:-translate-0.5"
+		>I understand and read everything, let me access the website.</button
+	>
 {/if}
